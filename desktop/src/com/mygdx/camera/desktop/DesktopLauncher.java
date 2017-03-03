870d6c31f1570b48570ae9bc5abefcc0d8017fc1
Adam Beltz <adambeltz@hotmail.com>
2017-03-03 13:48:39 -0600

    Initial commit

---
+++ desktop/src/com/mygdx/camera/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.camera.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.camera.Camera;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Camera(), config);
+		config.height = 1080;
+		config.width = 1200;
+	}
+}
